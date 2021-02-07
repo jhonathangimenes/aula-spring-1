8b243868d6b5ab69f5558856661683bf519b36b6
Jhonathan Gimenes <gimenesjhonathan@gmail.com>
2021-02-07 14:45:20 -0300

    exception findById

--- src/main/java/com/example/aulaspring1/services/UserService.java
+++ src/main/java/com/example/aulaspring1/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.example.aulaspring1.entities.User;
 import com.example.aulaspring1.repositories.UserRepository;
+import com.example.aulaspring1.services.exception.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
